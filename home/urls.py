779eb687aa8642801a611a0e44766800b91d25a5
ceciliabinck <cecilia4binck@gmail.com>
2020-11-01 22:05:31 +0000

    Added faq page with view and path and working link

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import how_works, about_us
+from .views import about_us, faq
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('how_works/', how_works, name="how_works"),
     path('about_us/', about_us, name="about_us"),
-
+    path('faq/', faq, name="faq"),
 ]
--- home/views.py
+++ home/views.py
@@ -19,3 +19,9 @@ def about_us(request):
     """ A view to return to the about us page """
 
     return render(request, "home/about_us.html")
+
+
+def faq(request):
+    """ A view to return to the FAQ page """
+
+    return render(request, "home/faq.html")
