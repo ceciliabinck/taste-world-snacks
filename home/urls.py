6aef29453350201635eec819e2c8aa9756c204f7
ceciliabinck <cecilia4binck@gmail.com>
2020-11-01 18:41:09 +0000

    Added view and path for about_us page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import how_works, about_us
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('how_works/', how_works, name="how_works"),
+    path('about_us/', about_us, name="about_us"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,15 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, "home/index.html")
+
+
+def how_works(request):
+    """ A view to return the how it works section on the index page """
+
+    return render(request, "home/index.html#works")
+
+
+def about_us(request):
+    """ A view to return to the about us page """
+
+    return render(request, "home/about_us.html")
