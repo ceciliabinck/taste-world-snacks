b4550a64ef88ab67655a1273a3a050fc6a0238cc
ceciliabinck <cecilia4binck@gmail.com>
2020-11-07 17:29:32 +0000

    Finished restoring product_detail def after mistaking it for all_products def

--- products/views.py
+++ products/views.py
@@ -44,10 +44,10 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
