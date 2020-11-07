d11840fb514180012dfefdf2ed67ed1f45dab4b7
ceciliabinck <cecilia4binck@gmail.com>
2020-11-07 14:53:13 +0000

    Added sorting to shop page

--- products/views.py
+++ products/views.py
@@ -1,27 +1,53 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
+from django.db.models.functions import Lower
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    categories = None
+    sort = None
+    direction = None
+
+    if 'sort' in request.GET:
+        sortkey = request.GET['sort']
+        sort = sortkey
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower('name'))
+
+    if 'direction' in request.GET:
+        direction = request.GET['direction']
+        if direction == 'desc':
+            sortkey = f'-{sortkey}'
+        products = products.order_by(sortkey)
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
-        'products': products
+        'products': products,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/shop.html', context)
 
 
 def product_detail(request, product_id):
-    """ A view to show individial product details """
+    """ A view to show individual product details """
 
-    product = get_object_or_404(Product, pk=product_id)
+    products = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product,
+        'products': products,
     }
 
     return render(request, 'products/product_detail.html', context)
