76f522545f68c195f460e6f38990a574753ec964
ceciliabinck <cecilia4binck@gmail.com>
2020-11-15 08:53:23 +0000

    Added countries to admin and link in footer

--- countries/admin.py
+++ countries/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Country
 
 # Register your models here.
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'about',
+        'image',
+    )
+
+
+admin.site.register(Country, CountryAdmin)
