25c72c794eddc9b89ec4297df3be86d450c06518
ceciliabinck <cecilia4binck@gmail.com>
2020-11-17 13:01:35 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': "pk_test_51HXU0lFxdNxFwpAqUcfktY9GhUSFC0i7rj5wBkEBpcp6MZGmK0hhnh7u1AAG8Tv3mAUFV0smWZkhQJyVkdlCaapl0079NMn7eK",
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
