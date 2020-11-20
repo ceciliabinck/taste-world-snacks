aad78529c76678bc06f56504b2091ec7e1b4e587
ceciliabinck <cecilia4binck@gmail.com>
2020-11-20 14:34:24 +0000

    added edit_product functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
+from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
@@ -77,3 +78,28 @@ def add_product(request):
     }
 
     return render(request, template , context)
+
+
+def edit_product(request, product_id):
+    """Edit a product in the store"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'Your are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
