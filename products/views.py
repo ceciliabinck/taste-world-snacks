f239f39d398e105e4bff9fc95c0535adc34400d0
ceciliabinck <cecilia4binck@gmail.com>
2020-11-20 13:48:22 +0000

    Finished add product functionality and added if statement on image bag page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from django.db.models.functions import Lower
 
 from .models import Product, Category
@@ -59,7 +60,17 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add a product to our store """
 
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = "products/add_product.html"
     context = {
         'form': form,
