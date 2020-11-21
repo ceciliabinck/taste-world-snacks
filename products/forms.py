ae05dbaeb11c2c79da42b4d8f307e9b75d02da98
ceciliabinck <cecilia4binck@gmail.com>
2020-11-21 19:33:31 +0000

    Corrected problems in products form.py

--- products/forms.py
+++ products/forms.py
@@ -9,12 +9,13 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(
+            label='image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
 
-        self.fields['category']
+        self.fields['category'].choices = categories
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
