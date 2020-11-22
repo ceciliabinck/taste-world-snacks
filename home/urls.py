771fae10f9065040b19a1fb94afd00e4502c90fa
ceciliabinck <cecilia4binck@gmail.com>
2020-11-22 22:38:10 +0000

    Corrected typo in name attibute in urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import about_us, faq
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('about_us/', about_us, name="about_us"),
-    path('faq/', faq, name="faq"),
+    path('', views.index, name='home'),
+    path('about_us/', about_us, name='about_us'),
+    path('faq/', faq, name='faq'),
 ]
