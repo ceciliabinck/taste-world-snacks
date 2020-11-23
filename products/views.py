cba261808a3dd2dc1b98ff97e6364fd5b83579d8
ceciliabinck <cecilia4binck@gmail.com>
2020-11-23 18:09:55 +0000

    Had an issue with migrating a form field and tried to change some things around

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Box(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Boxes'
+
+    name = models.CharField(max_length=254)
+    image = models.ImageField(null=False, blank=False)
+    time = models.DecimalField(max_digits=2, decimal_places=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
