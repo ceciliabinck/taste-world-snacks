a1a26f03208c5f45986f4588cbda7a2ec011c2db
ceciliabinck <cecilia4binck@gmail.com>
2020-11-24 15:35:09 +0000

    Added security to secret key and debug

--- taste_world_snacks/settings.py
+++ taste_world_snacks/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6@rsu7lc-vy84k!chxxj$gw6sjkj9wn6z&rbg4df2%9t9#=%a6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['taste-world-snacks.herokuapp.com', 'localhost']
 
