b965a6d1eaecc9d927d6f7d3ccfc8bf88e564ed0
ceciliabinck <cecilia4binck@gmail.com>
2020-11-24 12:39:20 +0000

    Deployment part 2 attempt 2

--- taste_world_snacks/settings.py
+++ taste_world_snacks/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '6@rsu7lc-vy84k!chxxj$gw6sjkj9wn6z&rbg4df2%9t9#=%a6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['taste-world-snacks.herokuapp.com', 'localhost']
 
