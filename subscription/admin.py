ec55be296d50bb8d1b17460c1a34abcd4d6349a4
ceciliabinck <cecilia4binck@gmail.com>
2020-11-25 10:19:57 +0000

    Adding box admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Box
 
 # Register your models here.
+
+
+class BoxAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'image'
+        'price',
+        'image_url',
+    )
+
+
+admin.site.register(Box, BoxAdmin)
