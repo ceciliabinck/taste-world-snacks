3752ee707335d50d869310346bd2909396ab66e2
ceciliabinck <cecilia4binck@gmail.com>
2020-11-26 21:14:36 +0000

    Updated the subscription models

--- subscription/models.py
+++ subscription/models.py
@@ -16,15 +16,10 @@ class Box(models.Model):
     class Meta:
         verbose_name_plural = 'Boxes'
 
-    length = models.ForeignKey('Length', on_delete=models.PROTECT)
     name = models.CharField(max_length=254)
     image = models.ImageField(upload_to='images', null=False, blank=False)
     about = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    month_price_1 = models.DecimalField(max_digits=6, decimal_places=2)
-    month_price_2 = models.DecimalField(max_digits=6, decimal_places=2)
-    month_price_3 = models.DecimalField(max_digits=6, decimal_places=2)
-
     image_url = models.URLField(max_length=1024, null=False, blank=False)
 
     def __str__(self):
