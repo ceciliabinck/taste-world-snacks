6506d16679828809e2f03e5cd73d68427a0a2931
ceciliabinck <cecilia4binck@gmail.com>
2020-11-27 10:20:22 +0000

    Added new super user to heroku and corrected styling sign up page

--- taste_world_snacks/settings.py
+++ taste_world_snacks/settings.py
@@ -121,6 +121,7 @@ WSGI_APPLICATION = 'taste_world_snacks.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
