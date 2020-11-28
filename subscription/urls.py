d17dab758dec87869f2adfe22f05fe45bf47c8b8
ceciliabinck <cecilia4binck@gmail.com>
2020-11-28 17:12:28 +0000

    Deleted subscription app as I decided to go through products for the boxes

--- subscription/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.box, name='box'),
-    path('box_length/<int:box_id>/', views.box_length, name='box_length'),
-]
